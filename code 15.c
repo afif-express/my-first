b10ce89687b483836b09788e9e04c29a286bb59e
afif-express <afif.likhon1696@gmail.com>
2025-03-24 01:12:55 +0600

    code 15.c

---
+++ code 15.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("All Formulas\n");
+    printf("1. Addition of Two Numbers = a+b\n");
+    printf("2. Subtraction of Two Numbers = a-b\n");
+    printf("3. Multiplication of Two Numbers = a*b\n");
+    printf("4. Division of Two Numbers = a/b\n");
+    printf("5. Modulus of Two Numbers = a%b\n");
+    printf("6. Area of Rectangle = a*b\n");
+    printf("7. Area of Triangle = sqrt(s(s-a)(s-b)(s-c))\n");
+    printf("8. Area of Circle = pi*r^2\n");
+    printf("9. Radius of Circle = sqrt(a^2+b^2)\n");
+    printf("10. Celcius to Fahrenheit = (9/5)*C+32\n");
+    printf("11. Inches to Centimeters = 2.54*I\n");
+    printf("12. Square Root of a Number = sqrt(a)\n");
+    printf("13. Square of a Number = a*a\n");
+    printf("14. Greatest of Three Numbers = a>b>c?a:b>c?b:c\n");
+    printf("15. Smalles of Three Numbers = a<b<c?a:b<c?b:c\n");
+    printf("16. Even or Odd = a%2==0?Even:Odd\n");
+    printf("17. Prime or Not = a>1?a%2==0?Not Prime:Prime:Not Prime\n");
+   
+
+
+
+    return 0;
+}
