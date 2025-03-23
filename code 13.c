b0a4d225df34276e3bbadf57997c1ea00c0e6496
afif-express <afif.likhon1696@gmail.com>
2025-03-24 00:49:25 +0600

    code 13.c

---
+++ code 13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int lenth,width,area;
+    printf("enter the value of lenth & width of the rectangle:");
+    scanf("%d %d",&lenth,&width);
+    area  =lenth*width ;
+    printf("Area is %2d",area);
+    return 0;
+}
